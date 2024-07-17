fdb62f1263f3fd240c2f3eee74c22a9051b6ed80
nandayekhaungkyaw <nandayekhaungkyaw>
2024-07-17 12:11:54 +0630

    phone-bug

--- main.js
+++ main.js
@@ -69,63 +69,7 @@ const mySecondAtropos = Atropos({
   // rest of parameters
 });
 
-import {
-  
-  observation2,
- 
-  observation6,
-} from "./src/observer.js";
-import {
-  aboutMeImg,
-  footer1,
-  footer2,
-  footer3,
-  footer4,
-  footer5,
-  frontend1,
-  frontend2,
-  left,
-  nav,
-  skill1,
-  skill2,
-  skill3,
-  skill4,
-  test,
-  ui1,
-  ui2,
-  ui3,
-  ui4,
-  ui5,
-} from "./src/selector.js";
-document.addEventListener("DOMContentLoaded", () => {
-  // observation();
-  // observation2(test, "animate__lightSpeedInLeft");
-  // observation2(aboutMeImg, "animate__lightSpeedInRight");
-  // observation2(nav, "animate__lightSpeedInLeft");
-  // observation2(skill1, "animate__lightSpeedInRight");
-  // observation2(skill2, "animate__lightSpeedInLeft");
-  // observation2(skill3, "animate__lightSpeedInRight");
-  // observation2(skill4, "animate__lightSpeedInLeft");
-  // observation2(ui1, "animate__lightSpeedInRight");
-  // observation2(ui2, "animate__lightSpeedInLeft");
-  // observation2(ui3, "animate__lightSpeedInRight");
-  // observation2(ui4, "animate__lightSpeedInLeft");
-  // observation2(ui5, "animate__lightSpeedInRight");
-  // observation2(frontend1, "animate__lightSpeedInLeft");
-  // observation2(frontend2, "animate__lightSpeedInRight");
-  // observation2(footer1, "animate__lightSpeedInLeft");
-  // observation2(footer2, "animate__lightSpeedInRight");
 
-  // observation2(footer3, "animate__lightSpeedInLeft");
-
-  // observation2(footer4, "animate__lightSpeedInRight");
-
-  // observation2(footer5, "animate__lightSpeedInLeft");
-
-  // // observation3();
-  // observation4();
-  // observation5()
-});
 
 const uiBtn = document.querySelector("#uiBtn");
 uiBtn.addEventListener("click", scroll);
